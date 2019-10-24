69977cc952857d054be075a01f2e2cfe64808e28
Derek Zhou <derekzhou1@gmail.com>
2019-10-24 10:45:06 -0400

    commit

--- main.cpp
+++ main.cpp
@@ -1,13 +1,12 @@
 #include <iostream>
 #define DOCTEST_CONFIG_IMPLEMENT
-#include "doctest.h"
 #include "funcs.h"
 using std::cout;
 using std::cin;
 using std::endl;
 using std::string;
 
-int main(int argc, char *argv[]){
+int main(){
     cout << removeLeadingSpaces("       int x = 5") <<endl;
 return 0;
 }
