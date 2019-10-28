730df1542b740b24472bdce4948f10989bce767b
herpderpisawesome <50245478+herpderpisawesome@users.noreply.github.com>
2019-10-27 22:13:32 -0400

    Delete funcs.h

--- funcs.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include <iostream>
-using std::string;
-
-string removeLeadingSpaces(string line);
-string rmindent(string line);
-int countChar(string line, char c);
-string indent(string line);
